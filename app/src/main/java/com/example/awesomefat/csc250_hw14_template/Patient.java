e513d9e41ca1d769e034c2dcf1693692c990cec7
Mouath Ibrahim <Mouath.Ibrahim@gmail.com>
2017-12-05 09:06:18 -0600

    HW14

--- app/src/main/java/com/example/awesomefat/csc250_hw14_template/Patient.java
+++ app/src/main/java/com/example/awesomefat/csc250_hw14_template/Patient.java
@@ -4,7 +4,7 @@ package com.example.awesomefat.csc250_hw14_template;
  * Created by awesomefat on 11/30/17.
  */
 
-public class Patient extends Person
+public class Patient extends Person implements Comparable<Patient>
 {
     private double balance;
 
@@ -17,5 +17,14 @@ public class Patient extends Person
     public String toString()
     {
         return String.format("%s Balance: %f", super.toString(), this.balance);
+    }
+
+    public int compareTo(Patient p) {
+        if(this.balance == p.balance)
+            return 0;
+        else if(this.balance < p.balance)
+            return -1;
+        else
+            return 1;
     }
 }
